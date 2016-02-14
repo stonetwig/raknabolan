894acb368180bb84567d3c7774d2a76591b8e7f8
Markus Stenqvist <daysnine@gmail.com>
2016-02-14 22:20:40 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,40 @@
+$(function() {
+  var resultbox = $('#resultbox');
+  resultbox.hide();
+
+  $('#calc').on('click', function(event) {
+    var amount = $('#amount').val().replace(/\s+/g, '').replace(',', '.') || 200000;
+    var interest = $('#interest').val().replace(/\s+/g, '').replace(',', '.') || 2;
+    var years = $('#years').val() || null;
+    var amort = $('#amort').val().replace(/\s+/g, '').replace(',', '.') || null;
+
+    var percentInterest = interest * 0.01;
+    var monthlyInterest = (amount * percentInterest) / 12;
+    var monthlyAmort = 0;
+
+    if (years !== null && (amort === null || !amort)) {
+      years = years ? years : 60;
+      monthlyAmort = Math.floor((amount / years) / 12);
+    } else if (amort && amort > 0) {
+      monthlyAmort = amort;
+    } else {
+      monthlyAmort = 0;
+    }
+
+    var total = Math.floor(monthlyInterest + monthlyAmort);
+    var description = (years !== null && (amort === null || !amort)) ?
+      "Med <strong>" + years + "</strong> års avbetalningsplan och en ränta på <strong>ca " + interest + "%</strong> så kommer du betala ca:" :
+      "Med en månadsammortering på <strong>" + monthlyAmort + " KR</strong> så kommer du betala ca:";
+
+    resultbox.html("<p>" + description + "</p><p class='resultNumber'>" + total + " KR i MÅNADEN</p><p>Varav " + monthlyInterest + "kr är ränta och " + monthlyAmort + "kr är ammortering</p>");
+    resultbox.show();
+  });
+
+  $('#years').focusout(function () {
+    if ($(this).val()) {
+      $('#amort').prop('disabled', true);
+    } else {
+      $('#amort').prop('disabled', false);
+    }
+  });
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+# räknabolån.se
+
+En liten sajt för att räkna utt ditt bolån eller lån generellt.
+Låter dig skriva in lånebelopp m.m. för att sedan presentera det enkelt för dig så du slipper räkna i huvudet själv.
+
+--->>> [räknabolån.se](http://xn--rknaboln-0zaq.se) <<<--- 
+
+Öppna issues för förslag / problem.
